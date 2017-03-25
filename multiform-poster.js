8e5c085b3345f3c24ec101bb2e1ea36148a3f9ef
Besto <manofthebag@gmail.com>
2017-03-25 11:13:35 -0400

    Create multiform-poster.js

---
+++ multiform-poster.js
@@ -0,0 +1,22 @@
+const rq = require('request');
+const fs = require('fs')
+const uri = `http://servIP:8080`
+const form = {
+	formData: {
+		chat_id: 237799109,
+		photo: {
+			value: fs.createReadStream("files/photo.jpg"),
+	        options: {
+	            filename: 'photo.jpg',
+	            contentType: 'image/jpg'
+	        }
+		}
+	}
+}
+
+rq.post(uri, form, function(err, res, data) {
+	if (err) console.log(err);
+	else {
+		console.log(data)
+	}
+})
